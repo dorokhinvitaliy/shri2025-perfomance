a3ec0da8c7968ca37fffbb31bfc214a1d44db85c
dorokhinvitaliy <dorokhin2006@bk.ru>
2025-07-05 15:48:40 +0400

    reacr removed

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { createRoot } from "react-dom/client";
-import { Header, Main } from "./App";
-
-createRoot(document.getElementById("root")).render(
-    <>
-        <Header />
-        <Main />
-    </>
-);
