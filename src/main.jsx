458ba13be4e2b01b57b3dd6415465168033b2cde
dorokhinvitaliy <dorokhin2006@bk.ru>
2025-07-05 15:11:37 +0400

    logic made easier

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from "react-dom/client";
+import { Header, Main } from "./App";
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-
-import App from './App';
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+createRoot(document.getElementById("root")).render(
+    <>
+        <Header />
+        <Main />
+    </>
+);
